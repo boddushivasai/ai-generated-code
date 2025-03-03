954295e7963e6794a14c8a169254b7e70e1c8142
shivasai2828 <boddushivasai@gmail.com>
2025-03-03 22:54:18 +0530

    AI Generated: generate function in python for finding prime numbers Generate Code

---
+++ server/git-repo/generated-code/code_1741022656830.py
@@ -0,0 +1,8 @@
+```python
+def generate_primes(n):
+    primes = []
+    for num in range(2, n+1):
+        if all(num % i != 0 for i in range(2, int(num**0.5)+1)):
+            primes.append(num)
+    return primes
+```
